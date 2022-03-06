094a49ca818f8d9efbcd4d93f99210cdb9b89585
miki4920 <miki4920@gmail.com>
2022-03-06 16:48:23 +0000

    Removed redundant files due to ability to derive all data from individual images

--- Submission.py
+++
@@ -1,8 +0,0 @@
-def create_submission(links, tags):
-    dictionary = {"links": links,
-                  "tags": tags,
-                  "name": "",
-                  "width": "",
-                  "height": "",
-                  "photos": []}
-    return dictionary
--- UtilityFunctions.py
+++
@@ -1,9 +0,0 @@
-import json
-import os
-
-
-def read_json(json_object):
-    if os.path.exists(json_object):
-        with open(json_object, 'rb') as file:
-            json_object = file.read()
-    return json.loads(json_object)
